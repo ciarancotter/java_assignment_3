c5acdd342c011b0e47cdcd1dd9d1014938ab8693
Ciarán Cotter <120326406@umail.ucc.ie>
2022-11-24 00:24:51 +0000

    Added the "Colour" constructor taking three floats: red, green and blue.

--- Colour.java
+++ Colour.java
@@ -1,2 +1,11 @@
 public class Colour {
+    float redColour;
+    float greenColour;
+    float blueColour;
+
+    public Colour(float red, float green, float blue){
+        redColour = red;
+        greenColour = green;
+        blueColour = blue;
+    }
 }
