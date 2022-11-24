54df2cc4fbeb0448821d74d94d7f4ca237ce20fb
ciarancotter <120326406@umail.ucc.ie>
2022-11-24 20:46:56 +0000

    Modified bit parsing

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -2,6 +2,9 @@ public class Colour {
     private float redColour;
     private float greenColour;
     private float blueColour;
+    private int redBits;
+    private int greenBits;
+    private int blueBits;
 
     public Colour(float red, float green, float blue){
         redColour = red;
@@ -9,15 +12,33 @@ public class Colour {
         blueColour = blue;
     }
 
-    public float getRedColour(){
+    public Colour(String bits){
+        redBits = Integer.parseInt(bits.substring(0, 8), 2);
+        greenBits = Integer.parseInt(bits.substring(8, 16), 2);
+        blueBits = Integer.parseInt(bits.substring(16, 24), 2);
+    }
+
+    public int getRedBits(){
+        return redBits;
+    }
+
+    public int getGreenBits(){
+        return greenBits;
+    }
+
+    public int getBlueBits(){
+        return blueBits;
+    }
+
+    public float getRedColourFloat(){
         return redColour;
     }
 
-    public float getGreenColour(){
+    public float getGreenColourFloat(){
         return greenColour;
     }
 
-    public float getBlueColour(){
+    public float getBlueColourFloat(){
         return blueColour;
     }
 }
--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -4,20 +4,38 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ColourTest {
 
     @Test
-    void testGetRedColour(){
+    void testGetRedColourFloat(){
         Colour colour = new Colour(1,0, 0);
-        assertEquals(1, colour.getRedColour());
+        assertEquals(1, colour.getRedColourFloat());
     }
 
     @Test
-    void testGetGreenColour(){
+    void testGetGreenColourFloat(){
         Colour colour = new Colour(0,1, 0);
-        assertEquals(1, colour.getGreenColour());
+        assertEquals(1, colour.getGreenColourFloat());
     }
 
     @Test
-    void testGetBlueColour(){
+    void testGetBlueColourFloat(){
         Colour colour = new Colour(0,0, 1);
-        assertEquals(1, colour.getBlueColour());
+        assertEquals(1, colour.getBlueColourFloat());
+    }
+
+    @Test
+    void testGetRedBits(){
+        Colour colour = new Colour("100000000000000000000000");
+        assertEquals(128, colour.getRedBits());
+    }
+
+    @Test
+    void testGetGreenBits(){
+        Colour colour = new Colour("000000001000000000000000");
+        assertEquals(128, colour.getGreenBits());
+    }
+
+    @Test
+    void testGetBlueBits(){
+        Colour colour = new Colour("000000000000000010000000");
+        assertEquals(128, colour.getBlueBits());
     }
 }
